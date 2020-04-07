ef4cde28b3a09222c0a46a0ad25f8c4fa910c16a
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-07 17:44:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@ class Dog
   def name=(name)
     @name = name 
   end 
-  def the_name
+  def name 
     @a_name
   end 
   def breed=(breed)
     @breed = breed 
   end 
-  def a_bread
+  def bread
     @breed 
   end
 end 
